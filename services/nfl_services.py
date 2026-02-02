dd25d49bf1f82287fbef22cfb3450aa72a0937d4
Anthony Moore <moore.32@hotmail.com>
2026-02-01 18:59:59 -0800

    Add nfl team defensive stats

--- services/nfl_services.py
+++ services/nfl_services.py
@@ -29,12 +29,52 @@ def get_nfl_team_offense_stats():
     },
     'penalties': data.get('pen'),
     'penalty_yards': data.get('pen yds'),
-    'differential': data.get('to diff')
+    'turnover_differential': data.get('to diff')
     }
     updated_data.append(modified_data)
   convert_data = json.dumps(updated_data, indent=2)
-
   return convert_data
+
+def get_nfl_team_defense_stats():
+  df = pd.read_csv('nfl_data/nfl_team_defense.csv')
+  df.columns = df.columns.str.lower()
+  team_defense_data = df.to_dict(orient='records')
+  updated_data = []
+
+  for data in team_defense_data:
+    modified_data = {
+      'rank': data.get('rank'),
+      'team': data.get('team'),
+      'total_points': data.get('pts'),
+      'total_plays': data.get('plays'),
+      'total_yards': data.get('yds'),
+      'yards_per_play': data.get('yds/play'),
+      'first_downs_allowed': data.get('1st dwn'),
+      'third_downs_allowed': {
+          'made': data.get('made'),
+          'attempts': data.get('att'),
+          'percentage': data.get('pct')
+      },
+      'red_zone_allowed': {
+          'made': data.get('made.1'),
+          'attempts': data.get('att.1'),
+          'percentage': data.get('pct.1')
+      },
+      'penalties': data.get('pen'),
+      'penalty_yards': data.get('pen yds'),
+      'turnover_differential': data.get('to diff')
+    }
+    updated_data.append(modified_data)
+  convert_data = json.dumps(updated_data, indent=2)
+  return convert_data
+
+def get_overall_weighted_defensive_average():
+  df = pd.read_csv('nfl_data/nfl_team_defense.csv')
+  df.columns = df.columns.str.lower()
+  total_teams = len(df)
+  total_yards_per_play_allowed = df['yds/play'].sum()
+  weighted_average = total_yards_per_play_allowed / total_teams
+  return weighted_average
 
 def get_nfl_player_snap_count():
   df = pd.read_csv('nfl_data/nfl_snap_count.csv')
