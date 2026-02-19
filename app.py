524461d8af8c787704a8e69d683a24cf63de5090
amoore3234 <akmoore@cpp.edu>
2026-02-18 17:13:23 -0800

    Training models to predict pga scores

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import services.nfl_services as nfl_services
+import services.pga_services as pga_services
 
 nfl_services.get_nfl_team_offense_stats()
 nfl_services.get_nfl_player_snap_count()
@@ -8,5 +9,6 @@ nfl_services.get_nfl_fantasy_points_leaders()
 nfl_services.get_nfl_dk_salary_data()
 nfl_services.get_nfl_odds()
 nfl_services.get_nfl_teams()
-print(f"Get projections: {nfl_services.get_nfl_fantasy_projections()}")
-# print(f"Get mascot names: {nfl_services.get_mascot_name()}")
+nfl_services.get_nfl_fantasy_projections()
+pga_services.player_expected_score()
+pga_services.player_expected_score_with_course_difficulty()
---
+++ services/pga_services.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+
+def player_expected_score():
+
+  data = {
+  'Player': ['Collin Morikawa', 'Collin Morikawa', 'Shane Lowry', 'Shane Lowry'],
+  'SG_Total': [7.938, 2.788, 1.25, 0.526]
+  }
+  df = pd.DataFrame(data)
+
+  player_stats = df.groupby('Player')['SG_Total'].agg(['mean', 'std']).reset_index()
+
+  course_rating = 73.4
+
+  player_stats[['Expected_Score', 'Ceiling', 'Floor']] = player_stats.apply(calculate_potential, rating=course_rating, axis=1)
+
+  print(player_stats[['Player', 'Expected_Score', 'Ceiling', 'Floor']])
+
+def player_expected_score_with_course_difficulty():
+  data = {
+  'Player': ['Collin Morikawa', 'Collin Morikawa', 'Shane Lowry', 'Shane Lowry'],
+  'SG_Total': [7.938, 2.788, 1.25, 0.526]
+  }
+  df = pd.DataFrame(data)
+
+  target_rating = 73.4
+  target_slope = 137
+
+  df['Tournament_Potential'] = df.apply(calculate_slope_potential, rating=target_rating, slope=target_slope, axis=1)
+  print(df[['Player', 'SG_Total', 'Tournament_Potential']])
+
+
+def calculate_slope_potential(row, rating, slope):
+  base_potential = rating - row['SG_Total']
+
+  slope_adjustment = slope / 113
+
+  adjusted_score = rating + ((base_potential - rating) * slope_adjustment)
+
+  return round(adjusted_score, 1)
+
+def calculate_potential(row, rating):
+  mean_sg = row['mean']
+  std_sg = row['std']
+
+  expected_score = rating - mean_sg
+
+  ceiling = expected_score - (2 * std_sg)
+  floor = expected_score + (2 * std_sg)
+
+  return pd.Series([round(expected_score, 1), round(ceiling, 1), round(floor, 1)])
+
