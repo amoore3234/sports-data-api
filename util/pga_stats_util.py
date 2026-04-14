497dfa38aaa0873b3e1bf46dc47d3a5890a4fef1
Anthony Moore <moore.32@hotmail.com>
2026-04-14 06:27:26 -0700

    Sorting scrambling data

--- util/pga_stats_util.py
+++ util/pga_stats_util.py
@@ -2,11 +2,14 @@ import pandas as pd
 
 # def generate_advanced_pga_stats():
 dk_salary_data = pd.read_csv('pga_data/dk_salaries.csv')
+data = pd.read_csv('pga_data/players_championships_stats.csv')
+df = pd.DataFrame(data)
 dk_salary_df = pd.DataFrame(dk_salary_data)
 golf_data = pd.read_csv('pga_data/golf_data.csv', encoding='cp1252')
 golf_data_df = pd.DataFrame(golf_data)
 golf_data_df = golf_data_df.iloc[:, ~golf_data_df.columns.str.contains('Unnamed')]
 golf_data_df = golf_data_df.replace(r'[\u2013\u2014]', 0, regex=True)
+golf_data_df = golf_data_df.replace('ï¿½', ' ')
 golf_data_df = golf_data_df.map(lambda x: x.title() if isinstance(x, str) else x)
 
 player_names = []
@@ -17,6 +20,7 @@ sg_off_the_tee = []
 sg_tee_to_green = []
 sg_total = []
 scrambling_statistic = []
+ids = []
 rounds = []
 
 round_one_results = list(golf_data_df['Round 1'])
@@ -26,12 +30,15 @@ round_four_results = list(golf_data_df['Round 4'])
 
 index = 0
 count = 0
+id = 0
 rounds_count = 0
 while index < len(round_one_results):
   player_name = round_one_results[index + 1]
-  player_name_array = player_name.split()
-  if player_name != 'Cut':
+  updated_name = player_name.replace('ï¿½', ' ')
+  player_name_array = updated_name.split()
+  if updated_name != 'Cut':
     player_first_last = f"{player_name_array[1]} {player_name_array[0]}"
+
     sg_putting.append(round_one_results[index + 4])
     sg_putting.append(round_two_results[index + 4])
     sg_putting.append(round_three_results[index + 4])
@@ -65,7 +72,9 @@ while index < len(round_one_results):
     while count < 4:
       player_names.append(player_first_last)
       rounds.append(rounds_count + 1)
+      ids.append(id + 1)
       scrambling_statistic.append(0)
+      id += 1
       rounds_count += 1
       count += 1
   index+=10
@@ -81,13 +90,18 @@ sg_statistics = {
   'SG Approach': sg_off_the_tee,
   'SG Tee To Green': sg_tee_to_green,
   'SG Total': sg_total,
-  'Scrambling': scrambling_statistic,
   'Rounds': rounds
 }
 
 sg_statistics_df = pd.DataFrame(sg_statistics)
-sg_statistics_df.to_csv('pga_data/latest_tournament_results.csv', index=False)
-print(f"SG Statistics: {sg_statistics_df}")
+statistics_df = sg_statistics_df.merge(
+  df,
+  left_on='Player',
+  right_on='Player',
+  how='left'
+)
+
+statistics_df.to_csv('pga_data/latest_tournament_results.csv', index=False)
 
 
 
