e0793da4b431803e21c7dbdbe2d2b691d856aec5
amoore3234 <akmoore@cpp.edu>
2026-04-15 15:54:39 -0700

    Fixed missing columns in data sets

--- util/pga_stats_util.py
+++ util/pga_stats_util.py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 
 # def generate_advanced_pga_stats():
-dk_salary_data = pd.read_csv('pga_data/dk_salaries.csv')
-data = pd.read_csv('pga_data/valspar_championship_stats.csv')
-df = pd.DataFrame(data)
-df['Player'] = df['Player'].str.lower()
-dk_salary_df = pd.DataFrame(dk_salary_data)
+merge_data_set = False
 golf_data = pd.read_csv('pga_data/golf_data.csv', encoding='cp1252')
 golf_data_df = pd.DataFrame(golf_data)
 golf_data_df = golf_data_df.iloc[:, ~golf_data_df.columns.str.contains('Unnamed')]
@@ -18,12 +14,15 @@ golf_data_df = golf_data_df.replace(' ', 0)
 name_map = {
   'min lee': 'min woo lee',
   'si kim': 'si woo kim',
-  'matt mccarty':'matthew mccarty',
+  'matt mccarty': 'matthew mccarty',
+  'iii capan': 'frankie capan iii',
   'rooyen van': 'erik van rooyen',
   'johnny keefer': 'john keefer',
   'zach bauchou': 'zachary bauchou',
   'de dumont': 'adrien dumont de chassart',
-  'harris english': 'harry english'
+  'johnny keefer': 'john keefer',
+  'zachary bauchou': 'zach bauchou',
+  'nico echavarria': 'nicolas echavarria'
 }
 
 player_names = []
@@ -104,7 +103,8 @@ sg_statistics = {
   'Player': player_names,
   'SG Putting': sg_putting,
   'SG Around Green': sg_around_green,
-  'SG Approach': sg_off_the_tee,
+  'SG Approach': sg_approach,
+  'SG Off The Tee': sg_off_the_tee,
   'SG Tee To Green': sg_tee_to_green,
   'SG Total': sg_total,
   'Rounds': rounds
@@ -112,15 +112,17 @@ sg_statistics = {
 
 sg_statistics_df = pd.DataFrame(sg_statistics)
 
-# print(f"Size of data frame: {(sg_statistics_df)}")
-# print(f"Data frame players: {df}")
-statistics_df = sg_statistics_df.merge(
-  df,
-  left_on='Player',
-  right_on='Player',
-  how='left'
-)
-if 'Scrambling' in statistics_df.columns:
+if merge_data_set:
+  data = pd.read_csv('pga_data/wm_phoenix_open_stats.csv')
+  df = pd.DataFrame(data)
+  df['Player'] = df['Player'].str.lower()
+
+  statistics_df = sg_statistics_df.merge(
+    df,
+    left_on='Player',
+    right_on='Player',
+    how='left'
+  )
   scrambling_list = list(statistics_df['Scrambling'])
   start = 0
   end = 4
@@ -137,5 +139,7 @@ if 'Scrambling' in statistics_df.columns:
     end += 16
 
   sg_statistics_df['Scrambling'] = new_scrambling
+  
 sg_statistics_df = sg_statistics_df.replace(r'^\s*$', np.nan, regex=True).fillna(0)
 sg_statistics_df.to_csv('pga_data/latest_tournament_results.csv', index=False)
+
